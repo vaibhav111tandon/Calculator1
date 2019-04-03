8742f79a79b7b334455703ff054608c936ec5ac9
shashank <shashankshukla137@gmail.com>
2019-04-03 14:48:24 +0530

    *TEST COMMIT*

---
+++ app/src/main/java/com/example/shash/first/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.shash.first;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText E1,E2;
+    Button B1,B2,B3,B4;
+    TextView T1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        E1=findViewById(R.id.num1);
+        E2=findViewById(R.id.num2);
+        B1=findViewById(R.id.button1);
+        B2=findViewById(R.id.button2);
+        B3=findViewById(R.id.button3);
+        B4=findViewById(R.id.button4);
+        T1=findViewById(R.id.textView);
+        B1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int num1,num2,res;
+                num1 = Integer.parseInt(E1.getText().toString());
+                num2=Integer.parseInt(E2.getText().toString());
+                res=num1+num2;
+                T1.setText(Integer.toString(res));
+            }
+        });
+        B2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int num1,num2,res;
+                num1 = Integer.parseInt(E1.getText().toString());
+                num2=Integer.parseInt(E2.getText().toString());
+                res=num1-num2;
+                T1.setText(Integer.toString(res));
+            }
+        });
+        B3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int num1,num2,res;
+                num1 = Integer.parseInt(E1.getText().toString());
+                num2=Integer.parseInt(E2.getText().toString());
+                res=num1*num2;
+                T1.setText(Integer.toString(res));
+            }
+        });
+        B4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int num1,num2;
+                double res;
+                num1 = Integer.parseInt(E1.getText().toString());
+                num2=Integer.parseInt(E2.getText().toString());
+                res=(double)num1/num2;
+                T1.setText(Double.toString(res));
+            }
+        });
+    }
+}
